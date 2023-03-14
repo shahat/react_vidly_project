749f195138169f25a612339844bdc897a2c1b5c5
shahat <me6847570@gmail.com>
2023-03-14 19:46:08 +0200

    this is the listing component from a service file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 import Movies from "./components/movies";
 import "./App.css";
-
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <Navbar />{" "}
+        <main className="container">
+          <Movies />
+        </main>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+        <nav class="navbar bg-body-tertiary bg-primary ">
+        <div class="container-fluid">
+          <a class="navbar-brand text-light" href="#">Navbar</a>
+        </div>
+      </nav>
+      
+    )
+  }
+}
+
+export default Navbar
